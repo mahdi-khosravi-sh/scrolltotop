05581b7e363e1dfdb1a2a3036871c9b19aaa107f
MahdiDev78 <MahdiDev78@gmail.com>
2021-08-30 08:28:17 +0430

    created demo recyclerview in the MainActivity

--- app/src/main/java/com/mahdikh/scrolltotop/MainActivity.java
+++ app/src/main/java/com/mahdikh/scrolltotop/MainActivity.java
@@ -1,8 +1,12 @@
 package com.mahdikh.scrolltotop;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mahdikh.scrolltotop.ui.RecyclerViewAdapter;
+import com.mahdikh.vision.scrolltotop.widget.ScrollToTop;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        ScrollToTop scrollToTop = findViewById(R.id.scrollToTop);
+
+        recyclerView.setAdapter(new RecyclerViewAdapter());
+        scrollToTop.setupWithRecyclerView(recyclerView);
     }
 }
---
+++ app/src/main/java/com/mahdikh/scrolltotop/ui/RecyclerViewAdapter.java
@@ -0,0 +1,41 @@
+package com.mahdikh.scrolltotop.ui;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mahdikh.scrolltotop.R;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textView.setText("Position : " + holder.getAdapterPosition());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 1000;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
