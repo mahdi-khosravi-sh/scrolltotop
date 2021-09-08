c6dd74fac916547f38abc0f7fc1c4fc8728f9be7
MahdiDev78 <MahdiDev78@gmail.com>
2021-09-08 21:00:51 +0430

    merged widgets

--- scrolltotop/src/main/java/com/mahdikh/vision/scrolltotop/widget/ScrollToTop.kt
+++ scrolltotop/src/main/java/com/mahdikh/vision/scrolltotop/widget/ScrollToTop.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.graphics.drawable.RippleDrawable
 import android.os.Build
 import android.util.AttributeSet
+import androidx.annotation.CallSuper
 import androidx.annotation.ColorInt
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatImageView
@@ -47,6 +48,14 @@ open class ScrollToTop : AppCompatImageView {
     var minimumScroll = 0
 
     /**
+     * اگر مقدار غیر از RecyclerView.NO_POSITION داشته باشد به این معناست
+     * که نما در صورتی نمایان شود که عمل اسکرول این position را رد کرده باشد
+     */
+    var minimumPosition = RecyclerView.NO_POSITION
+
+    var reverseLayout = false
+
+    /**
      * این متغییر جهت تشخیص وقفه و یا کامل شدن اسکرول با عمل کلیک ایجاد شده است
      * اگر روی نما کلیک شود مقدار true می گیرد و در صورتی که در حین اسکرول توسط عمل Dragging
      * اسکرول متقف شود یا اگر اسکرول کامل شد مقدار false خواهد گرفت
@@ -60,7 +69,7 @@ open class ScrollToTop : AppCompatImageView {
      */
     private var callFlag = true
 
-    protected var recyclerView: RecyclerView? = null
+    private var recyclerView: RecyclerView? = null
 
     var animator: BaseAnimator? = FadeAnimator(0.8f)
         set(value) {
@@ -117,9 +126,35 @@ open class ScrollToTop : AppCompatImageView {
 
     private fun initialization(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
         val a = context.obtainStyledAttributes(attrs, R.styleable.ScrollToTop, 0, defStyleAttr)
-        isHeavyCheckup = a.getBoolean(R.styleable.ScrollToTop_heavyCheckup, false)
-        isSmoothScroll = a.getBoolean(R.styleable.ScrollToTop_smoothScroll, false)
-        minimumScroll = a.getDimensionPixelOffset(R.styleable.ScrollToTop_minimumScroll, 250)
+        val count = a.indexCount
+        var index: Int
+        for (i in 0 until count) {
+            index = a.getIndex(i)
+            when (index) {
+                R.styleable.ScrollToTop_minimumPosition -> {
+                    minimumPosition = a.getInt(index, RecyclerView.NO_POSITION)
+                }
+                R.styleable.ScrollToTop_heavyCheckup -> {
+                    isHeavyCheckup = a.getBoolean(
+                        R.styleable.ScrollToTop_heavyCheckup,
+                        false
+                    )
+                }
+                R.styleable.ScrollToTop_smoothScroll -> {
+                    isSmoothScroll = a.getBoolean(
+                        R.styleable.ScrollToTop_smoothScroll,
+                        false
+                    )
+                }
+                R.styleable.ScrollToTop_minimumScroll -> {
+                    minimumScroll = a.getDimensionPixelOffset(
+                        R.styleable.ScrollToTop_minimumScroll,
+                        250
+                    )
+                }
+            }
+        }
+
         if (!a.hasValue(R.styleable.ScrollToTop_android_src) && !a.hasValue(R.styleable.ScrollToTop_srcCompat)) {
             setImageResource(R.drawable.ic_scroll_top)
         }
@@ -184,20 +219,38 @@ open class ScrollToTop : AppCompatImageView {
     }
 
     protected open fun computeCurrentScroll(): Int {
+        if (reverseLayout) {
+            recyclerView?.let {
+                return it.computeVerticalScrollRange() - (it.computeVerticalScrollExtent()
+                        + recyclerView!!.computeVerticalScrollOffset())
+            }
+        }
         return recyclerView!!.computeVerticalScrollOffset()
     }
 
     protected open fun checkupScroll() {
-        val newScroll: Int = computeCurrentScroll()
-        if (newScroll >= minimumScroll && callFlag) {
-            show()
-            callFlag = false
-        } else if (newScroll < minimumScroll && !callFlag) {
-            hide()
-            callFlag = true
+        if (minimumPosition != RecyclerView.NO_POSITION) {
+            val firstVisible = getFirstVisiblePosition()
+            if (firstVisible > minimumPosition && callFlag) {
+                show()
+                callFlag = false
+            } else if (firstVisible <= minimumPosition && !callFlag) {
+                hide()
+                callFlag = true
+            }
+        } else {
+            val newScroll: Int = computeCurrentScroll()
+            if (newScroll >= minimumScroll && callFlag) {
+                show()
+                callFlag = false
+            } else if (newScroll < minimumScroll && !callFlag) {
+                hide()
+                callFlag = true
+            }
         }
     }
 
+    @CallSuper
     protected open fun startScroll() {
         if (performedClick) {
             return
@@ -228,6 +281,7 @@ open class ScrollToTop : AppCompatImageView {
         animator?.onStartScroll(this)
     }
 
+    @CallSuper
     protected open fun prepare() {
         animator?.onPrepare(this)
         recyclerView?.let {
@@ -235,23 +289,39 @@ open class ScrollToTop : AppCompatImageView {
         }
     }
 
-    protected fun show() {
+    @CallSuper
+    protected open fun show() {
         animator?.onShow(this) ?: kotlin.run {
             alpha = 1.0F
             visibility = VISIBLE
         }
     }
 
-    protected fun hide() {
+    @CallSuper
+    protected open fun hide() {
         animator?.onHide(this) ?: kotlin.run {
             visibility = GONE
         }
     }
 
+    @CallSuper
     open fun setupWithRecyclerView(recyclerView: RecyclerView) {
         this.recyclerView = recyclerView
         prepare()
         recyclerView.addOnScrollListener(scrollListener)
+        reverseLayout = isRecyclerViewReverseLayout()
+    }
+
+    private fun isRecyclerViewReverseLayout(): Boolean {
+        val manager = recyclerView?.layoutManager
+        if (manager != null) {
+            if (manager is LinearLayoutManager) {
+                return manager.reverseLayout
+            } else if (manager is StaggeredGridLayoutManager) {
+                return manager.reverseLayout
+            }
+        }
+        return false
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -277,13 +347,5 @@ open class ScrollToTop : AppCompatImageView {
     override fun performClick(): Boolean {
         startScroll()
         return super.performClick()
-    }
-
-    protected fun setCallFlag(onceCall: Boolean) {
-        this.callFlag = onceCall
-    }
-
-    protected fun getCallFlag(): Boolean {
-        return callFlag
     }
 }
